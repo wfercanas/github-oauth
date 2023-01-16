37cdb48f9965d272ab7ad90a121e3d4e5eea21e1
wfercanas <fernando.canas@outlook.com>
2023-01-16 17:26:33 -0500

    feat: conditional render if accessToken exists

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [accessToken, setAccessToken] = useState();
+
+  if (accessToken) {
+    return (
+      <div>
+        <h3>Logged In</h3>
+        <p>
+          <a href="?action=repos">View Repos</a>
+        </p>
+        <p>
+          <a href="?action=logout">Log Out</a>
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <p>Hola</p>
+      <h3>Not Logged In</h3>
+      <p>
+        <a href="?action=login">Log In</a>
+      </p>
     </div>
   );
 }
